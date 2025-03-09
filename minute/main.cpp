ec8ea84382385fb744e2167a5dcf580953d86a91
shaovoon <shaovoon@yahoo.com>
2025-03-09 15:18:28 +0800

    First commit

---
+++ minute/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "unittest.h"
+
+void CompareNumbers()
+{
+    int num1 = 12;
+	int num2 = 14;
+	CHECK(num1, == , num2);
+
+	CHECK(++num1, == , ++num2);
+}
+
+void CheckExceptionThrown()
+{
+	int num = 2;
+	CHECK(2, == , num);
+
+    CHECK_EXP_THROW(2 == 2, std::runtime_error);
+	CHECK_EXP_THROW(throw std::runtime_error("error"), std::runtime_error);
+	CHECK_EXP_THROW(3 == 3, std::runtime_error);
+}
+
+int main()
+{
+	UnitTest::SetResultFilePath("d:\\temp2\\unit_tests.txt"); // optional
+    UnitTest::Add("AppSuite", "CompareNumbers", CompareNumbers);
+	UnitTest::Add("AppSuite", "CheckExceptionThrown", CheckExceptionThrown);
+	
+	// RunAllTests() return number of errors
+    return UnitTest::RunAllTests();
+}
---
+++ minute/unittest.h
@@ -0,0 +1,214 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <memory>
+#include "conv.h"
+
+class UnitTest
+{
+public:
+	UnitTest() = default;
+	static void Add(const std::string& suite, const std::string& testName, std::function<void()> test)
+	{
+		TestList[suite].push_back(std::make_pair(testName, test));
+	}
+	// return number of errors
+	static int RunAllTests()
+	{
+		int errors = 0;
+		int local_errors = 0;
+		for (auto& list : TestList)
+		{
+			local_errors = 0;
+			printf("[%s] running\n", list.first.c_str());
+			if (File)
+				fprintf(File, "[%s] running\n", list.first.c_str());
+
+			for (auto& pr : list.second)
+			{
+				printf("%s: running\n", pr.first.c_str());
+				if (File)
+					fprintf(File, "%s: running\n", pr.first.c_str());
+
+				try
+				{
+					Error = false;
+					pr.second();
+					if (Error == false)
+					{
+						printf("%s: \033[92mpassed!\033[0m\n", pr.first.c_str());
+						if (File)
+							fprintf(File, "%s: passed!\n", pr.first.c_str());
+					}
+					else
+					{
+						printf("%s: \033[91mfailed!\033[0m\n", pr.first.c_str());
+						if (File)
+							fprintf(File, "%s: failed!\n", pr.first.c_str());
+						++local_errors;
+					}
+				}
+				catch (std::exception& e)
+				{
+					++local_errors;
+					fprintf(stderr, "Unknown Exception thrown while testing %s, Exception message: %s\n", pr.first.c_str(), e.what());
+					if (File)
+						fprintf(File, "Unknown Exception thrown while testing %s, Exception message: %s\n", pr.first.c_str(), e.what());
+				}
+				catch (...)
+				{
+					++local_errors;
+					fprintf(stderr, "Unknown Exception thrown while testing %s\n", pr.first.c_str());
+					if (File)
+						fprintf(File, "Unknown Exception thrown while testing %s\n", pr.first.c_str());
+				}
+			}
+			errors += local_errors;
+			size_t local_count = list.second.size();
+
+			printf("\n[%s] ", list.first.c_str());
+			if (File)
+				fprintf(File, "\n[%s] ", list.first.c_str());
+
+			printf("%s%zu / %zu passed!\033[0m\n", GetClr(local_errors == 0), local_count - local_errors, local_count);
+			if (File)
+				fprintf(File, "%zu / %zu passed!\n", local_count - local_errors, local_count);
+		}
+		size_t count = CountTests();
+
+		printf("%s\nTotal: %zu / %zu passed!\033[0m\n", GetClr(errors == 0), count - errors, count);
+
+		if (File)
+			fprintf(File, "\nTotal: %zu / %zu passed!\n", count - errors, count);
+
+		if (errors == 0)
+		{
+			printf("\n\033[92mALL PASSED!\033[0m\n");
+			if (File)
+				fprintf(File, "\nALL PASSED!\n");
+		}
+		if (File)
+		{
+			fflush(File);
+			fclose(File);
+			File = nullptr;
+		}
+		return errors;
+	}
+	static const char* GetClr(bool pass)
+	{
+		return pass ? "\033[92m" : "\033[91m";
+	}
+	static void SetError(bool error)
+	{
+		Error = error;
+	}
+	static size_t CountTests()
+	{
+		size_t count = 0;
+		for (auto& list : TestList)
+		{
+			count += list.second.size();
+		}
+		return count;
+	}
+	static void Check(bool result, const char* op1, 
+		const char* compare, const char* op2, 
+		const std::string& a, const std::string& b, 
+		const char* file, int line_num, const char* func)
+	{
+		if (result == false)
+		{
+			UnitTest::SetError(true);
+			printf("    %s (line #%d): %s:", file, line_num, func);
+			printf("CHECK(%s %s %s) \033[91mfailed\033[0m: ", op1, compare, op2);
+			printf("(%s %s %s)\n", a.c_str(), compare, b.c_str());
+
+			if (File)
+			{
+				fprintf(File, "    %s (line #%d): %s:", file, line_num, func);
+				fprintf(File, "CHECK(%s %s %s) failed: ", op1, compare, op2);
+				fprintf(File, "(%s %s %s)\n", a.c_str(), compare, b.c_str());
+			}
+		}
+	}
+	static bool SetResultFilePath(const char* path)
+	{
+		if (File)
+		{
+			fclose(File);
+			File = nullptr;
+		}
+		File = fopen(path, "wt");
+
+		return File != nullptr;
+	}
+	static void CloseFile()
+	{
+		if (File)
+		{
+			fclose(File);
+			File = nullptr;
+		}
+	}
+	static bool IsFileOpen()
+	{
+		return File != nullptr;
+	}
+	static FILE* GetFilePtr()
+	{
+		return File;
+	}
+	static void Print(const char* text)
+	{
+		printf("%s", text);
+		if (File)
+		{
+			fprintf(File, "%s", text);
+		}
+	}
+private:
+	static inline FILE* File = nullptr;
+	static inline bool Error = false;
+	static inline std::unordered_map<std::string,
+	std::vector<std::pair<std::string, std::function<void()> > > > TestList;
+};
+
+#if defined(__GNUC__)
+#define MINUTE_FUNCTION __PRETTY_FUNCTION__
+#elif defined(_MSC_VER)
+#define MINUTE_FUNCTION __FUNCSIG__
+#else
+#define MINUTE_FUNCTION __func__
+#endif
+
+#define CHECK(op1, compare, op2)                                                 \
+    {   auto v1 = op1; auto v2 = op2;                                            \
+        UnitTest::Check((v1 compare v2), #op1, #compare, #op2,                   \
+                        conv::to_str(v1), conv::to_str(v2), __FILE__, __LINE__, MINUTE_FUNCTION); \
+    }
+
+#define CHECK_EXP_THROW(op, exception)              \
+{                                                   \
+    bool thrown = false;                            \
+    try                                             \
+    {                                               \
+        op;                                         \
+    }                                               \
+    catch (exception ex)                            \
+    {                                               \
+	    thrown = true;                              \
+    }                                               \
+    if (thrown == false)                            \
+    {                                               \
+        char buf[2000];                             \
+        snprintf(buf, sizeof(buf), "    %s (line #%d): CHECK_EXP_THROW(%s, %s) did not throw in %s\n",  \
+             __FILE__, __LINE__,  #op, #exception, MINUTE_FUNCTION); \
+        UnitTest::Print(buf);                       \
+        UnitTest::SetError(true);                   \
+    }                                               \
+}
+
